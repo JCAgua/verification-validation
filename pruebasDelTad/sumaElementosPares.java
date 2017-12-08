4701eac34d972b76d7743b2d1284487883c2555b
JuanCarlos <juancarlos.aguag@gmail.com>
2017-12-08 21:44:11 +0100

    Hipotesis 1
    Añadiendo numero impar 3

--- pruebasDelTad/sumaElementosPares.java
+++ pruebasDelTad/sumaElementosPares.java
@@ -11,7 +11,9 @@ public class sumaElementosPares {
 		int elem, resul;
 		if (! pila.pilaVacia ()) {
 			elem = pila.desapilar ();
-			resul = elem + sumar(pila);
+                        //if(elem%2==0)
+                            resul = elem + sumar(pila);
+                        //else resul = sumar(pila);
 			pila.apilar (elem);
 		}
 		else resul = 0;
@@ -20,11 +22,16 @@ public class sumaElementosPares {
 	public static void main (String [ ] args) throws PilaVacia {
 		Pila pila1 = new TadPila(); 
 		int i, num;	
-		System.out.println();	
-		for (i= 1; i<= 10; i++) 
-			if(i%2==0)
-			   pila1.apilar(i);
-		       num = sumar(pila1);	
-		    System.out.println("Numero de elementos: " + num);	
+		System.out.println();
+                for(i=1; i<=10; i++)
+                    if(i%2==0)
+                        pila1.apilar(i);
+                pila1.apilar(3);
+                /*pila1.apilar(10);
+                pila1.apilar(21);
+                pila1.apilar(54);
+                pila1.apilar(100);*/
+		num = sumar(pila1);	
+		System.out.println("Numero de elementos: " + num);	
 	}
 }
